00444d0ec3b02447114bcbac54f07a298a8ef976
trgiang14 <23010474@st.phenikaa-uni.edu.vn>
2025-10-30 18:20:00 +0700

    cau truc lai main, sua home thanh player va them trang home+about

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,61 @@
 import 'package:flutter/material.dart';
+import 'screens/player_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/about_screen.dart';
 
 void main() {
   runApp(FlowApp());
 }
 
-class FlowApp extends StatelessWidget {
+class FlowApp extends StatefulWidget {
   const FlowApp({super.key});
+
+  @override
+  State<FlowApp> createState() => _FlowAppState();
+}
+
+class _FlowAppState extends State<FlowApp> {
+  int _selectedIndex = 0;
+
+  //Danh sách các trang
+  final List<Widget> _screen = const [
+    HomeScreen(),
+    PlayerScreen(),
+    AboutScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flow Music',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.deepPurple),
-      home: const HomeScreen(),
+      title: "Flow Up",
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.blue,
+      ),
+      darkTheme: ThemeData.dark(),
+      home: Scaffold(
+        body: _screen[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.music_note),
+              label: 'Player',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.info_outline),
+              label: 'About',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,75 +1,10 @@
-import 'package:flow_up/widgets/player_controls.dart';
 import 'package:flutter/material.dart';
-import '/widgets/song_progress_bar.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurple.shade50,
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple.shade100,
-        elevation: 0,
-        title: const Text(
-          'Flow Music',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //Ảnh bài hát
-            Container(
-              height: 280,
-              width: 280,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.deepPurple.withOpacity(0.3),
-                    blurRadius: 20,
-                    offset: const Offset(0, 10),
-                  ),
-                ],
-                image: const DecorationImage(
-                  image: AssetImage(
-                    'images/pexels-julias-torten-und-tortchen-434418-31001122.jpg',
-                  ),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            const SizedBox(height: 24),
-            //Tên bài hát+ca sĩ
-            const Text(
-              'Night Changes',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 4),
-            Text(
-              'One Dỉrection',
-              style: TextStyle(color: Colors.grey.shade600, fontSize: 16),
-            ),
-            const SizedBox(height: 32),
-            //Thanh tiến trình
-            const SongProgressBar(current: 60, total: 180),
-            const SizedBox(height: 32),
-
-            //Nút điều khiển
-            const PlayerControls(),
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
